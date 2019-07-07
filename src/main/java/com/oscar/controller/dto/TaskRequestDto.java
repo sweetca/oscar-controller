14604ee5545e1ab8582954d9f1a1424e89b4d6e9
Alex Skorohod <alexey.skorohod@gmail.com>
2019-07-07 21:55:29 +0200

    Simplify JobType for jackson

--- src/main/java/com/oscar/controller/dto/TaskRequestDto.java
+++ src/main/java/com/oscar/controller/dto/TaskRequestDto.java
@@ -5,13 +5,21 @@ import com.oscar.controller.model.job.JobType;
 import lombok.Data;
 
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 @Data
 public class TaskRequestDto {
 
     private Component component;
 
-    private TreeSet<JobType> jobConfig = new TreeSet<>();
+    private TreeSet<Integer> types = new TreeSet<>();
+
+    public TreeSet<JobType> getJobConfig() {
+        return types
+                .stream()
+                .map(JobType::fromType)
+                .collect(Collectors.toCollection(TreeSet::new));
+    }
 
     public String isValid() {
         if (component == null) {
@@ -21,7 +29,7 @@ public class TaskRequestDto {
         if (componentValid != null) {
             return componentValid;
         }
-        if (jobConfig == null || jobConfig.size() == 0) {
+        if (types == null || types.size() == 0) {
             return "Not valid pipeline config!";
         }
         return null;
